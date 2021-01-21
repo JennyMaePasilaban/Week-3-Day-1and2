f405713a8d3dd39755b2e4d034a64e85e4024359
JennyMaePasilaban <76983461+JennyMaePasilaban@users.noreply.github.com>
2021-01-21 09:20:00 +0800

    Add files via upload

---
+++ Week 3 Day 1 & 2.css
@@ -0,0 +1,32 @@
+<html>
+<Head> 
+<title> WEEK3: Day1 & 2 </title> 
+</head>
+<body>
+<Body Bgcolor = "yellow">
+<Font face = verdana, size= "3">
+<u> The world is now suffering <br> from corona virus </u>
+<br> Let us always pray for our health. <br> <br>
+<p align= "center"> <img src= "Week 3/sun.jpg", width= 210, length= 210> </img> <br>
+The sunshine is so bright <br>
+It gives hope to everyone <br> <br>
+<img src= "Week 3/circle.jpg", width= 300, length= 300> </img>
+<br> It's a small <br>
+<b> World after all </b> <br> <br> </p>
+<p align= "right"> <img src= "Week 3/smiley.jpg", width= 450,length= 450> </img> <br>
+<b> Always wear a smile, </b> <br> you might inspire somebody <br> with it.</font> </p>
+<br> <br> <br>
+<Font face = verdana, color= "red", size= "1">
+<i> "Beginning today, I will create a new future by creating a new me. </font>
+<br> <br> <Font color= "black", size= "2"> No longer will I dwell in a pit of despair, </font>
+<br> <br> <Font color= "red", size= "3"> moaning over squandered time and lost oppurtunity. </i> </font>
+<br> <br> <Font color= "black", size= "4"> I can do nothing about my past. My future is immediate.</font>
+<br> <br> <Font color= "red", size= "5"> I will grasp it in both hands and carry it with running feet.</font>
+<br> <br> <Font color= "black", size= "6"> When I am faced with the choice of doing nothing or doing something, </font>
+<br> <br> <Font color= "red", size= "7"> I will always choose to act! I seize this moment. </font>
+<br> <br> <Font color= "black", size= "7"> I choose now." ~ </font>
+<br> <br> <Font color= "red", size= "3"> Andy Andrews </font>
+<br> <br> <p align= "right"> <img src= "Week 3/bluegirl.jpg", width= 210, length= 210> </p>
+<Font face = verdana, color= "blue", size= "10"> <p align= "center"> Jenny Mae M. Pasilaban </font> </p>
+</body>
+</html>
